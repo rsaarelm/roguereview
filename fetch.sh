18677f012d605acf49795615151a1ffe34fa08bd
Risto Saarelma <risto.saarelma@iki.fi>
2016-10-18 18:48:22 +0300

    Update fetch links

--- fetch.sh
+++ fetch.sh
@@ -25,9 +25,10 @@ get http://downloads.sourceforge.net/ivan/ivan-0.50.tar.gz
 get http://homepages.cwi.nl/~aeb/games/hack/hack-1.0.3.tar.gz
 get http://downloads.sourceforge.net/gearhead/gearhead/1.100/gh-1100-src.zip
 get http://sourceforge.net/projects/avanor/files/avanor/0.5.8/avanor-0.5.8-src.tar.bz2
-get http://jonathanscorner.com/download/tms2_0.tar.gz
+get https://cjshayward.com/wp-content/sourcecode/tms2_0.tar.gz
 get http://downloads.sourceforge.net/project/sc2/UQM/0.7/uqm-0.7.0-source.tgz
 get http://pesky-reticulans.org/game/prime_src-2.4.tar.bz2
+get http://nethack4.org/media/releases/nethack4-4.3-beta2.zip
 
 if [ ! -f cataclysm2.zip ]
 then
@@ -44,9 +45,7 @@ unzip -o cache/gh-1100-src.zip
 mkdir -p moria; cd moria; unzip -o ../cache/mor552cs.zip; cd -
 unzip -o cache/rogue-libc5-ncurses.zip; tar xzvf rogue-libc5-ncurses.tar.gz; rm rogue-libc5-ncurses.tar.gz
 
-if [ ! -d nethack4 ]; then git clone git://gitorious.org/nitrohack/ais523.git nethack4; fi
-
-if [ ! -d incursion ]; then git clone https://github.com/rmtew/incursion-roguelike.git incursion; fi
+if [ ! -d incursion ]; then git clone https://bitbucket.org/rmtew/incursion-roguelike.git incursion; fi
 
 if [ ! -d ja2-stracciatella ]; then git clone https://bitbucket.org/gennady/ja2-stracciatella.git; fi
 
